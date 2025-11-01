3a7fc446f51f636be135e757e45d49892c2d9948
Raja Babu <111038560+Raja-718@users.noreply.github.com>
2025-11-01 16:25:44 +0530

    ChatModel using Hugging Face api key

---
+++ 4.chatmodel_hf_api.py
@@ -0,0 +1,43 @@
+
+'''from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task="text-generation"
+
+)
+model = ChatHuggingFace(llm=llm)
+result = model.invoke("What is the capital of bihar")
+
+print(result.content)'''
+
+
+'''from langchain_huggingface import HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task="text-generation"
+)
+
+result = llm.invoke("What is the capital of Bihar?")
+print(result)'''
+
+from langchain_huggingface import HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task="text-generation",
+    provider="default" 
+)
+
+result = llm.invoke("What is the capital of Bihar?")
+print(result)
